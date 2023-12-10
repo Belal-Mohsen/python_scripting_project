e44ed3faebde77cc22d6840e6ba13d8c4427d9c7
Belal-Mohsen <belal4mohsen@gmail.com>
2023-12-09 20:09:11 -0500

    Fixed an error

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def get_all_apps_dirs(source):
 
     for root, dirs, files in os.walk(source):
         for directory in dirs:
-            if DIR_JAVA in directory:
+            if DIR_JAVA in directory.lower():
                 java_apps_paths.append(os.path.join(source, directory))
-            elif DIR_GO in directory:
+            elif DIR_GO in directory.lower():
                 go_apps_paths.append(os.path.join(source, directory))
         break
     return java_apps_paths, go_apps_paths
@@ -27,6 +27,7 @@ def main(source, dest):
     source_path = os.path.join(cwd, source)
     dest_path = os.path.join(cwd, dest)
     java_paths, go_paths = get_all_apps_dirs(source_path)
+    print(java_paths, "\n", go_paths)
 
 
 if __name__ == "__main__":
